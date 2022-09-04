01f9ae1cdb5c6b29e8a92f5321e8eb4c5cb1f496
mramirid <amir.muh.hakim@gmail.com>
2022-09-04 19:25:11 +0700

    refactor: throw the correct error instance

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,7 @@ import "./index.css";
 
 const rootElement = document.getElementById("root");
 if (rootElement === null) {
-  throw new Error("The root element is undefined");
+  throw new TypeError("The root element is undefined");
 }
 
 ReactDOM.createRoot(rootElement).render(
